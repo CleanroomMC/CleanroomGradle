ec1e53e8dd187d8416b3e15a1321cc5d37a910c3
Rongmario <rongmario@protonmail.com>
2022-03-20 18:19:34 +0000

    runClient + runServer tasks

--- src/main/java/com/cleanroommc/gradle/CleanroomGradlePlugin.java
+++ src/main/java/com/cleanroommc/gradle/CleanroomGradlePlugin.java
@@ -7,6 +7,7 @@ import com.google.gson.GsonBuilder;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.dsl.RepositoryHandler;
+import org.gradle.api.tasks.JavaExec;
 
 import static com.cleanroommc.gradle.Constants.*;
 
@@ -47,14 +48,43 @@ public class CleanroomGradlePlugin implements Plugin<Project> {
             });
         });
 
-        CleanroomLogger.log2("Setting up Minecraft DSL Block...", project.getDisplayName());
+        CleanroomLogger.log2("Setting up Minecraft DSL Block...");
         project.getExtensions().create(MINECRAFT_EXTENSION_KEY, MinecraftExtension.class);
         MinecraftExtension mcExt = MinecraftExtension.get(project);
         CleanroomLogger.log2("      runDir: \"{}\"", mcExt.getRunDir());
         CleanroomLogger.log2("      clientJvmArgs: {}", mcExt.getClientJvmArgs());
         CleanroomLogger.log2("      serverJvmArgs: {}", mcExt.getServerJvmArgs());
 
+        CleanroomLogger.log2("Setting up client run task...");
+        JavaExec runClient = project.getTasks().create("runClient", JavaExec.class);
+        runClient.getOutputs().dir(mcExt.getRunDir());
+        runClient.doFirst(task -> ((JavaExec) task).setWorkingDir(mcExt.getRunDir()));
+        runClient.setStandardOutput(System.out);
+        runClient.setErrorOutput(System.err);
+        runClient.setDescription("Runs Minecraft's Client");
+        CleanroomLogger.log2("Setting up server run task...");
+        JavaExec runServer = project.getTasks().create("runServer", JavaExec.class);
+        runServer.getOutputs().dir(mcExt.getRunDir());
+        runServer.doFirst(task -> ((JavaExec) task).setWorkingDir(mcExt.getRunDir()));
+        runServer.setStandardInput(System.in);
+        runServer.setStandardOutput(System.out);
+        runServer.setErrorOutput(System.err);
+        runServer.setDescription("Runs Minecraft's Server");
+        /*
+                    JavaExec exec = makeTask("runClient", JavaExec.class);
+            exec.getOutputs().dir(delayedFile(REPLACE_RUN_DIR));
+            exec.setMain(GRADLE_START_CLIENT);
+            exec.doFirst(task -> ((JavaExec) task).workingDir(delayedFile(REPLACE_RUN_DIR)));
+            exec.setStandardOutput(System.out);
+            exec.setErrorOutput(System.err);
 
+            exec.setGroup("ForgeGradle");
+            exec.setDescription("Runs the Minecraft client");
+
+            exec.doFirst(makeRunDir);
+
+            exec.dependsOn("makeStart");
+         */
 
         currentProject = project;
     }
