2dcc208f6d29d7c836fee487d97597761e17b40b
Rongmario <rongmario@protonmail.com>
2024-03-08 16:18:20 +0000

    Remove patches references from LoaderDevExtension, ready for decoupling

--- workspace/src/main/java/com/cleanroommc/gradle/env/loaderdev/LoaderDevExtension.java
+++ workspace/src/main/java/com/cleanroommc/gradle/env/loaderdev/LoaderDevExtension.java
@@ -1,7 +1,5 @@
 package com.cleanroommc.gradle.env.loaderdev;
 
-import org.gradle.api.file.DirectoryProperty;
-import org.gradle.api.file.ProjectLayout;
 import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Nested;
@@ -14,18 +12,12 @@ public abstract class LoaderDevExtension {
     public static final String PROPERTY_NAME = "com.cleanroommc.loader-dev.extension";
 
     @Inject
-    public abstract ProjectLayout getProjectLayout();
-
-    @Inject
     public abstract ObjectFactory getObjectFactory();
 
     @Nested
     public abstract Property<LoaderDevTasks> getTasks();
 
-    public abstract DirectoryProperty getPatchesFolder();
-
     public LoaderDevExtension() {
-        getPatchesFolder().convention(getProjectLayout().getProjectDirectory().dir("patches"));
         getTasks().convention(getObjectFactory().newInstance(LoaderDevTasks.class));
     }
 
