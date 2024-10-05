84b40705b7ca087f2b2f2fb54d8712f30252f647
Rongmario <rongmario@proton.me>
2024-10-05 01:27:22 +0100

    Make configurations lazy and not instantly resolved again

--- src/main/java/com/cleanroommc/gradle/api/named/Configurations.java
+++ src/main/java/com/cleanroommc/gradle/api/named/Configurations.java
@@ -1,29 +1,29 @@
 package com.cleanroommc.gradle.api.named;
 
+import org.gradle.api.NamedDomainObjectProvider;
 import org.gradle.api.Project;
 import org.gradle.api.UnknownDomainObjectException;
 import org.gradle.api.artifacts.Configuration;
 
 import java.util.Collection;
 
-// Configuration is not lazy at this moment in time for gradle, hence we resolve it straight away
 public final class Configurations {
 
     public static Collection<Configuration> all(Project project) {
         return project.getConfigurations();
     }
 
-    public static Configuration of(Project project, String name, boolean transitive) {
+    public static NamedDomainObjectProvider<Configuration> of(Project project, String name, boolean transitive) {
         var configuration = of(project, name);
-        configuration.setTransitive(transitive);
+        configuration.configure(c -> c.setTransitive(transitive));
         return configuration;
     }
 
-    public static Configuration of(Project project, String name) {
+    public static NamedDomainObjectProvider<Configuration> of(Project project, String name) {
         try {
-            return project.getConfigurations().named(name).get();
+            return project.getConfigurations().named(name);
         } catch (UnknownDomainObjectException ignored) { }
-        return project.getConfigurations().register(name).get();
+        return project.getConfigurations().register(name);
     }
 
     private Configurations() { }
