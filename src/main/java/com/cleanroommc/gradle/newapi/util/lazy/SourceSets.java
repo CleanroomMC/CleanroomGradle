af798bc96d8af26c4c9ae6ad26bd918f921c8019
Rongmario <rongmario@proton.me>
2025-02-20 18:18:01 +0000

    Add more SourceSet helpers

--- src/main/java/com/cleanroommc/gradle/newapi/util/lazy/SourceSets.java
+++ src/main/java/com/cleanroommc/gradle/newapi/util/lazy/SourceSets.java
@@ -1,7 +1,11 @@
 package com.cleanroommc.gradle.newapi.util.lazy;
 
+import com.cleanroommc.gradle.newapi.task.Tasks;
+import com.cleanroommc.gradle.newapi.util.Objects;
+import org.gradle.api.Action;
 import org.gradle.api.NamedDomainObjectProvider;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.file.SourceDirectorySet;
 import org.gradle.api.plugins.JavaPluginExtension;
@@ -9,6 +13,7 @@ import org.gradle.api.provider.Provider;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.SourceSetContainer;
 import org.gradle.api.tasks.SourceSetOutput;
+import org.gradle.api.tasks.compile.JavaCompile;
 
 import java.io.File;
 import java.util.List;
@@ -27,13 +32,28 @@ public final class SourceSets {
         $.configure(sourceSet -> sourceSet.getJava().setSrcDirs(List.of(sourceDir)));
     }
 
+    public static void extendFromConfiguration(Project project, NamedDomainObjectProvider<SourceSet> $, NamedDomainObjectProvider<Configuration> configuration) {
+        $.configure(sourceSet -> {
+            var config = Objects.resolvedConfig(project, sourceSet.getImplementationConfigurationName());
+            config.extendsFrom(configuration.get());
+        });
+    }
+
+    public static Provider<String> compile(NamedDomainObjectProvider<SourceSet> sourceSet) {
+        return sourceSet.map(SourceSet::getCompileJavaTaskName);
+    }
+
+    public static void configureCompile(Project $1, NamedDomainObjectProvider<SourceSet> $2, Action<JavaCompile> action) {
+        $1.afterEvaluate(project -> $2.configure(sourceSet -> Tasks.<JavaCompile>named(project, sourceSet.getCompileJavaTaskName()).configure(action)));
+    }
+
     public static Provider<File> source(NamedDomainObjectProvider<SourceSet> sourceSet) {
         return sourceSet.map(SourceSet::getAllJava)
                 .map(SourceDirectorySet::getSourceDirectories)
                 .map(FileCollection::getSingleFile);
     }
 
-    public static Provider<File> compiledClasses(NamedDomainObjectProvider<SourceSet> sourceSet) {
+    public static Provider<File> classes(NamedDomainObjectProvider<SourceSet> sourceSet) {
         return sourceSet.map(SourceSet::getOutput)
                 .map(SourceSetOutput::getClassesDirs)
                 .map(FileCollection::getSingleFile);
