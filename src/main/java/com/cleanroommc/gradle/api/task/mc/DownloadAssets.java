e2d5c56cfbfa2014f5e091026f9c8a7307025e66
Rongmario <rongmario@proton.me>
2026-09-11 02:25:00 +0100

    chore: relicense under CleanroomMC License 1.0

--- src/main/java/com/cleanroommc/gradle/api/task/mc/DownloadAssets.java
+++ src/main/java/com/cleanroommc/gradle/api/task/mc/DownloadAssets.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2022-2026 CleanroomMC contributors
+ *
+ * This file is licensed under the CleanroomMC License Version 1.0.
+ * See the applicable LICENSE file in this directory or a parent directory
+ * for the full licence terms.
+ *
+ * This is visible-source software and is not open-source software.
+ */
+
 package com.cleanroommc.gradle.api.task.mc;
 
 import com.cleanroommc.gradle.api.Meta;
